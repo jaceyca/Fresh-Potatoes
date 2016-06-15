2791f7b9008f34bafe211bdd595d7fe03680cddd
jaceyca <choi.jess816@gmail.com>
2016-06-15 14:29:01 -0700

    First 2 required functionalities
    User can view a list of movies currently playing in theaters from The
    Movie Database.
    Poster images are loaded using the UIImageView category in the
    AFNetworking library.

---
+++ Pods/Target Support Files/Pods-Fresh Potatoes/Pods-Fresh Potatoes-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Fresh_Potatoes : NSObject
+@end
+@implementation PodsDummy_Pods_Fresh_Potatoes
+@end
---
+++ Pods/Target Support Files/Pods-Fresh Potatoes/Pods-Fresh Potatoes-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Fresh_PotatoesVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Fresh_PotatoesVersionString[];
+
